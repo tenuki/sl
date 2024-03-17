18776c069f73486d65d2e6278348113585953c77
david weil <david.weil@endlesstruction.com.ar>
2024-03-17 08:35:23 +0000

    removed unused

--- front/src/config/constants.ts
+++
@@ -1,9 +0,0 @@
-import { Constants } from '~/types';
-
-const constants: Constants = {
-  // Put your project constants here
-};
-
-export const getConstants = (): Constants => {
-  return constants;
-};
